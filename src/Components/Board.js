d8e52fd1c66cbfc43fa234afabbc04f738169253
keyurparalkar <keur.plkar@gmail.com>
2020-11-03 23:11:00 +0530

    Added OnClick Fade for PickSide Comp. and UI Alignment changes to Board Comp.

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -22,41 +22,41 @@ class Board extends React.Component {
         const square = this.state.boardState.slice();
         let isXVal = this.state.isX;
 
-        if(this.calculateWinner(square) || square[i]){
+        if (this.calculateWinner(square) || square[i]) {
             return;
         }
-        if(this.state.isX){
+        if (this.state.isX) {
             square[i] = 'X';
-            isXVal=false;
-        } else{
+            isXVal = false;
+        } else {
             square[i] = 'O';
-            isXVal=true;
+            isXVal = true;
         }
-        
-        this.setState({boardState: square, isX: isXVal});
+
+        this.setState({ boardState: square, isX: isXVal });
     }
 
-    renderSquare(i){
-        return(
-            <Square value={this.state.boardState[i]} onClick={()=>this.togglePlayerMarker(i)} />
+    renderSquare(i) {
+        return (
+            <Square value={this.state.boardState[i]} onClick={() => this.togglePlayerMarker(i)} />
         );
     }
 
-    calculateWinner(squares){
+    calculateWinner(squares) {
         const lines = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
         ];
-        
-        for(let i=0;i < lines.length; i++){
+
+        for (let i = 0; i < lines.length; i++) {
             let [a, b, c] = lines[i];
-            if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
                 return squares[a];
             }
         }
@@ -64,17 +64,17 @@ class Board extends React.Component {
         return null;
     }
 
-    generateRandomNumber(min, max){
-        let num = Math.floor(Math.random()*(max-min)) + min;
-        return num === 0 ? this.generateRandomNumber(min, max): num;
+    generateRandomNumber(min, max) {
+        let num = Math.floor(Math.random() * (max - min)) + min;
+        return num === 0 ? this.generateRandomNumber(min, max) : num;
     }
 
-    computerTurn(){
+    computerTurn() {
         //Getting blank array pos:
         let blank_pos = this.state.boardState.map((val, index) => val === null ? index : 0).filter((val) => val !== 0);
         let min_pos = blank_pos[0];
-        let max_pos = blank_pos[blank_pos.length -1];
-        console.log('blank_pos = ',blank_pos)
+        let max_pos = blank_pos[blank_pos.length - 1];
+        console.log('blank_pos = ', blank_pos)
         //Generate random number
         let num = this.generateRandomNumber(min_pos, max_pos);
 
@@ -84,8 +84,8 @@ class Board extends React.Component {
         return num;
     }
 
-    componentDidUpdate(){
-        if(this.state.isX === false){
+    componentDidUpdate() {
+        if (this.state.isX === false) {
             let x = this.computerTurn();
             console.log(x);
             this.togglePlayerMarker(x);
@@ -94,32 +94,32 @@ class Board extends React.Component {
 
     render() {
         const winner = this.calculateWinner(this.state.boardState);
-        let status = winner ? `Winner: ${winner}` : this.state.isX ? `Current Player: X`:`Current Player: O`;
+        let status = winner ? `Winner: ${winner}` : this.state.isX ? `Current Player: X` : `Current Player: O`;
 
-        
+
 
         return (
-            <div>
-            <h1> Tic-Tac-Toe </h1>  
-            <span>{status}</span>
-            <div className="board">
-                <div className="row-1">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="row-2">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="row-3">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+            <div className="board-container">
+                <h1> Tic-Tac-Toe </h1>
+                <span>{status}</span>
+                <div className="board">
+                    <div className="row-1">
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                    </div>
+                    <div className="row-2">
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                    </div>
+                    <div className="row-3">
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                    </div>
 
-            </div>
+                </div>
             </div>
         );
     }
--- src/Components/PickSide.js
+++ src/Components/PickSide.js
@@ -4,24 +4,50 @@ import "./StyleSheets/PickSide.css";
 import O from './Icons/o.svg';
 import x_mark from './Icons/x-mark.svg';
 
-export const PickSide = (props) => {
-    return(
+export class PickSide extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state={
+            radioSelect:null
+        };
+
+        this.handleRadioClick = this.handleRadioClick.bind(this);
+        this.radio1 = React.createRef();
+        this.radio2 = React.createRef();
+        this.marker1 = React.createRef();
+        this.marker2 = React.createRef();
+
+    }
+
+    handleRadioClick(event){
+        if(event.currentTarget === this.radio1.current){
+            this.marker1.current.style.opacity = 1;
+            this.marker2.current.style.opacity = 0.5;
+        } else if(event.currentTarget === this.radio2.current){
+            this.marker2.current.style.opacity = 1;
+            this.marker1.current.style.opacity = 0.5
+        }
+    }
+
+    render() {
+
+        return(
         <div className="pickside-container">
             <h1>Pick your side</h1>
             <div className="marker-icons">
                 <div className="marker-1">
-                    <img src={x_mark} alt="x-marker"/>
-                    <input type="radio"/>
+                    <img src={x_mark} alt="x-marker" ref={this.marker1}/>
+                    <input name="pickSide-radiogroup" type="radio" ref={this.radio1} onClick={this.handleRadioClick}/>
                 </div>
-                
+
                 <div className="marker-2">
-                    <img src={O} alt="o-marker"/>
-                    <input type="radio"/>
+                    <img src={O} alt="o-marker" ref={this.marker2}/>
+                    <input name="pickSide-radiogroup" type="radio" ref={this.radio2} onClick={this.handleRadioClick}/>
                 </div>
-                
+
             </div>
             <button>Continue</button>
-        </div>
-        
-    );
+        </div>)
+    }
 }
